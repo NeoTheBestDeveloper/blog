06d8ac7a607e277d27a522096c49a9a727a8caea
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-02-24 12:12:40 +0300

    test has been succesed

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
           </div>
         </div>
         <div className={s.Menu_bottom}>
-          <div className={s.Menu_adaptive}>Recipes & LOL</div>
+          <div className={s.Menu_adaptive}>Recipes & Search</div>
           <ul className={s.Menu_items}>
             <li className={s.item}>
               <a href=".">All recipes</a>
