b8d9843aec1ebd0f6edcf7a8240a941c9d7672ff
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-02-24 10:43:54 +0300

    make Header and Menu adaptive and add plug for pop-up menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,25 +3,23 @@ import s from './Header.module.css';
 const Header = () => {
   return (
     <div className={s.Header}>
-      <div>
-        <ul className={s.Headers_items}>
-          <li className={s.item}>
-            <a href=".">About</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Cookbook</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Shop</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Subscribe</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Saved</a>
-          </li>
-        </ul>
-      </div>
+      <ul className={s.Headers_items}>
+        <li className={s.item}>
+          <a href=".">About</a>
+        </li>
+        <li className={s.item}>
+          <a href=".">Cookbook</a>
+        </li>
+        <li className={s.item}>
+          <a href=".">Shop</a>
+        </li>
+        <li className={s.item}>
+          <a href=".">Subscribe</a>
+        </li>
+        <li className={s.item}>
+          <a href=".">Saved</a>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -3,38 +3,43 @@ import logo from './../../img/logo.png';
 
 const Menu = () => {
   return (
-    <div className={s.Menu}>
-      <div className={s.Menu_top}>
-        <div className={s.Menu_logo}>
-          <img src={logo} alt="" />
+    <div className={s.Menu_wrapper}>
+      <div className={s.Menu}>
+        <div className={s.Menu_top}>
+          <div className={s.Menu_logo}>
+            <img src={logo} alt="" />
+          </div>
         </div>
-      </div>
-      <div className={s.Menu_bottom}>
-        <ul className={s.Menu_items}>
-          <li className={s.item}>
-            <a href=".">All recipes</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Breakfast</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Salad</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Soup</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Dinner</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Dessert</a>
-          </li>
-          <li className={s.item}>
-            <a href=".">Favorites</a>
-          </li>
-        </ul>
-        <div className={s.Menu_search}>
-          <input type="search" placeholder="Search..." />
+        <div className={s.Menu_bottom}>
+          <div className={s.Menu_adaptive}>Recipes & searh</div>
+          <ul className={s.Menu_items}>
+            <li className={s.item}>
+              <a href=".">All recipes</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Breakfast</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Salad</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Soup</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Dinner</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Dessert</a>
+            </li>
+            <li className={s.item}>
+              <a href=".">Favorites</a>
+            </li>
+            <li className={s.item}>
+              <div className={s.Menu_search}>
+                <input type="search" placeholder="Search..." />
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
