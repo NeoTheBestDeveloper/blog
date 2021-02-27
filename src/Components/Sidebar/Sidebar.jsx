8117bf1dc18f14a79d480f47e2e63946aa3ba5d5
NeoTheBestDeveloper <neothebestmain@protonmail.com>
2021-02-27 14:37:12 +0300

    add subscribe form in sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,16 +1,18 @@
 import s from './Sidebar.module.css';
-import neo from './../../img/Neo.jpg';
+import neo from './../../img/neo.jpg';
 
 const Sidebar = () => {
   return (
     <div className={s.Sidebar}>
+
+
       <div className={s.aboutMe}>
         <div className={s.photo}>
           <img src={neo} alt="" />
         </div>
         <div className={s.decribtions}>
           Hello! I cook fresh, vegetarian recipes. My dog Cookie, catches the crumbs. About C+K »
-        </div>
+         </div>
         <div className={s.social}>
           <div className={s.social_top}>
             <div className={s.item}>
@@ -35,11 +37,22 @@ const Sidebar = () => {
             <div className={s.item}></div>
           </div>
         </div>
-      </div>
-      <div className={s.myCookbook}></div>
-      <div className={s.subscribe}></div>
-      <div className={s.popularRecipes}></div>
-    </div>
+      </div> {/* AboutMe */}
+
+      <div className={s.subscribe}>
+        <div className={s.title}>Don’t miss a new recipe</div>
+        <form className={s.subscribeForm} action="">
+          <input type="text" placeholder="Email adress..." />
+          <button type="submit">Subscribe</button>
+        </form>
+      </div> {/* Subscribe */}
+
+      <div className={s.popularRecipes}>
+
+      </div> {/* PopularRecipes */}
+
+
+    </div> /* Sidebar */
   );
 };
 
